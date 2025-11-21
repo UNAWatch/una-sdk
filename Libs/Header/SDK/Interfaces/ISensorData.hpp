318424d20430f7665656db1f990dadb0a643f579
Oleksandr Tymoshenko <oleksandr.tymoshenko@droid-technologies.com>
2025-11-21 20:36:41 +0200

    added us support to the sensor layer data

--- Libs/Header/SDK/Interfaces/ISensorData.hpp
+++ Libs/Header/SDK/Interfaces/ISensorData.hpp
@@ -21,6 +21,7 @@ class ISensorData
 {
 public:
     virtual uint32_t getTimestamp()                 const = 0;
+    virtual uint64_t getTimestampUs()               const = 0;
     virtual uint16_t getLength()                    const = 0;
     virtual float    getAsFloat(uint16_t index = 0) const = 0;
     virtual uint32_t getAsU32(uint16_t index = 0)   const = 0;
