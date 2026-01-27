826355af58bbf5fa206bbc996f6f05633d441213
avatarsd <avatarsd2@gmail.com>
2026-01-27 10:09:39 +0200

    build(cmake): simplify SDK path handling with environment variables
    Refactor una-app.cmake to remove internal SDK path detection and always
    require UNA_SDK environment variable. Update all paths to use $ENV{UNA_SDK}
    directly for consistency. Simplify build functions by removing redundant
    parameters and setups, assuming external configuration for paths like
    LIBS_PATH and TOUCHGFX_GUI_PATH. Add debug messages for key variables in
    service and GUI builds. Update una-sdk.cmake to use environment variable
    for all source and include paths. Adjust OUTPUT_PATH handling and copy
    commands to use OUTPUT_DIR list.

--- cmake/una-app.cmake
+++ cmake/una-app.cmake
@@ -1,25 +1,17 @@
 # UNA App CMake Project Configuration
-# This file contains common CMake logic extracted from app CMakeLists.txt
-# to enable both relative (within SDK) and environment-variable based (external) SDK referencing
 
-# Determine SDK path - check if we're within SDK or external
-if(EXISTS "${CMAKE_CURRENT_LIST_DIR}/../tools/una.py")
-    # We're within the SDK repository
-    set(UNA_SDK "${CMAKE_CURRENT_LIST_DIR}/..")
-    message("Building within SDK repository: ${UNA_SDK}")
-else()
-    # External app - require UNA_SDK environment variable
-    if(NOT DEFINED ENV{UNA_SDK})
-        message(FATAL_ERROR "UNA_SDK environment variable must be set for external apps")
-    endif()
-    set(UNA_SDK "$ENV{UNA_SDK}")
-    message("Using external SDK: ${UNA_SDK}")
+if(NOT DEFINED ENV{UNA_SDK})
+    message(FATAL_ERROR "UNA_SDK environment variable must be set for external apps")
+endif()
+if(NOT DEFINED OUTPUT_PATH)
+    set(OUTPUT_PATH ${CMAKE_SOURCE_DIR}/build)
+endif()
+if(NOT DEFINED RESOURCES_PATH)
+    set(RESOURCES_PATH "${CMAKE_CURRENT_SOURCE_DIR}/Resources")
 endif()
 
-include("${UNA_SDK}/cmake/una-sdk.cmake")
-
 # Common toolchain setup
-set(CMAKE_TOOLCHAIN_FILE "${UNA_SDK}/cmake/toolchain-arm-none-eabi.cmake")
+set(CMAKE_TOOLCHAIN_FILE $ENV{UNA_SDK}/cmake/toolchain-arm-none-eabi.cmake)
 
 # Set standards
 set(CMAKE_C_STANDARD 11)
@@ -52,7 +44,7 @@ set(UNA_APP_COMMON_LINK_OPTIONS
     -nostdlib
     -static
     -Wl,--emit-relocs
-    -Wl,-L${UNA_SDK}/Utilities/Scripts/linker
+    -Wl,-L$ENV{UNA_SDK}/Utilities/Scripts/linker
     -mcpu=cortex-m33
     -mfpu=fpv5-sp-d16
     -mfloat-abi=hard
@@ -98,9 +90,7 @@ function(una_app_setup_version BUILD_VERSION_OUT WORKING_DIR)
 endfunction()
 
 # Function to build service executable
-function(una_app_build_service TARGET_NAME LIBS_PATH TOUCHGFX_GUI_PATH)
-    include("${LIBS_PATH}/libs.cmake")
-
+function(una_app_build_service TARGET_NAME)
     if(NOT DEFINED UNA_APP_SERVICE_STACK_SIZE)
         if(DEFINED SERVICE_STACK_SIZE)
             set(UNA_APP_SERVICE_STACK_SIZE "${SERVICE_STACK_SIZE}")
@@ -116,25 +106,14 @@ function(una_app_build_service TARGET_NAME LIBS_PATH TOUCHGFX_GUI_PATH)
         endif()
     endif()
 
-    # Service sources - app-specific + common SDK sources
-    set(SERVICE_SOURCES
-        ${LIBS_SOURCES}
-        "${CMAKE_CURRENT_SOURCE_DIR}/syscalls.cpp"
-        ${UNA_SDK_SOURCES_COMMON}
-        ${UNA_SDK_SOURCES_SERVICE}
-    )
+    # Print variable values to highlight no hidden dependencies
+    message("UNA_APP_SERVICE_STACK_SIZE: ${UNA_APP_SERVICE_STACK_SIZE}")
+    message("UNA_APP_SERVICE_RAM_LENGTH: ${UNA_APP_SERVICE_RAM_LENGTH}")
+    message("APP_ID: ${APP_ID}")
+    message("APP_NAME: ${APP_NAME}")
+    message("DEV_ID: ${DEV_ID}")
 
     add_executable(${TARGET_NAME} ${SERVICE_SOURCES})
-
-    set(SERVICE_INCLUDE_DIRS
-        ${LIBS_INCLUDE_DIRS}
-        ${UNA_SDK_INCLUDE_DIRS_COMMON}
-        ${UNA_SDK_INCLUDE_DIRS_SERVICE}
-    )
-
-    if(EXISTS "${TOUCHGFX_GUI_PATH}/gui/include")
-        list(APPEND SERVICE_INCLUDE_DIRS "${TOUCHGFX_GUI_PATH}/gui/include")
-    endif()
 
     target_include_directories(${TARGET_NAME} PRIVATE ${SERVICE_INCLUDE_DIRS})
 
@@ -147,29 +126,27 @@ function(una_app_build_service TARGET_NAME LIBS_PATH TOUCHGFX_GUI_PATH)
 
     target_link_libraries(${TARGET_NAME} PRIVATE
         -Wl,--start-group
-        ${UNA_SDK}/libc++/libstdc++.a
+        $ENV{UNA_SDK}/libc++/libstdc++.a
         -Wl,--end-group
     )
 
     target_link_options(${TARGET_NAME} PRIVATE
-        -T "${UNA_SDK}/Utilities/Scripts/linker/Main/Sections.ld"
+        -T "$ENV{UNA_SDK}/Utilities/Scripts/linker/Main/Sections.ld"
         -Wl,--defsym=STACK_SIZE=${UNA_APP_SERVICE_STACK_SIZE}
         -Wl,--defsym=RAM_LENGTH=${UNA_APP_SERVICE_RAM_LENGTH}
-        -Wl,-Map=${CMAKE_BINARY_DIR}/${TARGET_NAME}.elf.map
+        -Wl,-Map=${OUTPUT_PATH}/${TARGET_NAME}.elf.map
         ${UNA_APP_COMMON_LINK_OPTIONS}
-        -L${UNA_SDK}/libc++
+        -L$ENV{UNA_SDK}/libc++
     )
 
     add_custom_command(TARGET ${TARGET_NAME} POST_BUILD
-        COMMAND python3 ${UNA_SDK}/Utilities/Scripts/app_packer/app_packer.py -e $<TARGET_FILE:${TARGET_NAME}> -o ${CMAKE_CURRENT_BINARY_DIR}/Tmp -ext srv
+        COMMAND python3 $ENV{UNA_SDK}/Utilities/Scripts/app_packer/app_packer.py -e $<TARGET_FILE:${TARGET_NAME}> -o ${CMAKE_CURRENT_BINARY_DIR}/Tmp -ext srv
         COMMENT "Packing ${TARGET_NAME}"
     )
 endfunction()
 
 # Function to build GUI executable
-function(una_app_build_gui TARGET_NAME LIBS_PATH TOUCHGFX_GUI_PATH)
-    include("${TOUCHGFX_GUI_PATH}/touchgfx.cmake")
-
+function(una_app_build_gui TARGET_NAME)
     if(NOT DEFINED UNA_APP_GUI_STACK_SIZE)
         if(DEFINED GUI_STACK_SIZE)
             set(UNA_APP_GUI_STACK_SIZE "${GUI_STACK_SIZE}")
@@ -185,122 +162,51 @@ function(una_app_build_gui TARGET_NAME LIBS_PATH TOUCHGFX_GUI_PATH)
         endif()
     endif()
 
-    set(GUI_SOURCES
-        ${TOUCHGFX_SOURCES}
-        "${CMAKE_CURRENT_SOURCE_DIR}/PaintImpl.cpp"
-        "${CMAKE_CURRENT_SOURCE_DIR}/syscalls.cpp"
-        ${UNA_SDK_SOURCES_COMMON}
-        ${UNA_SDK_SOURCES_GUI}
-    )
+    message("UNA_APP_GUI_STACK_SIZE: ${UNA_APP_GUI_STACK_SIZE}")
+    message("UNA_APP_GUI_RAM_LENGTH: ${UNA_APP_GUI_RAM_LENGTH}")
+
+    # Compute library directories from TOUCHGFX_LIBS
+    set(TOUCHGFX_LIBS_DIRS "")
+    foreach(lib IN LISTS TOUCHGFX_LIBS)
+        get_filename_component(lib_dir "${lib}" DIRECTORY)
+        list(APPEND TOUCHGFX_LIBS_DIRS "-L${lib_dir}")
+    endforeach()
+    list(REMOVE_DUPLICATES TOUCHGFX_LIBS_DIRS)
 
     add_executable(${TARGET_NAME} ${GUI_SOURCES})
 
-    target_include_directories(${TARGET_NAME} PRIVATE
-        ${LIBS_INCLUDE_DIRS}
-        ${UNA_SDK_INCLUDE_DIRS_COMMON}
-        ${UNA_SDK_INCLUDE_DIRS_GUI}
-        ${TOUCHGFX_INCLUDE_DIRS}
-    )
+    target_include_directories(${TARGET_NAME} PRIVATE ${GUI_INCLUDE_DIRS})
 
     target_link_libraries(${TARGET_NAME} PRIVATE
         -Wl,--start-group
-        ${UNA_SDK}/libc++/libstdc++.a
-        ${TOUCHGFX_GUI_PATH}/touchgfx/lib/core/cortex_m33/gcc/libtouchgfx-float-abi-hard.a
+        $ENV{UNA_SDK}/libc++/libstdc++.a
+        ${TOUCHGFX_LIBS}
         -Wl,--end-group
     )
 
     target_link_options(${TARGET_NAME} PRIVATE
-        -L${TOUCHGFX_GUI_PATH}/touchgfx/lib/core/cortex_m33/gcc
-        -T "${UNA_SDK}/Utilities/Scripts/linker/Main/Sections.ld"
+        ${TOUCHGFX_LIBS_DIRS}
+        -T "$ENV{UNA_SDK}/Utilities/Scripts/linker/Main/Sections.ld"
         -Wl,--defsym=STACK_SIZE=${UNA_APP_GUI_STACK_SIZE}
         -Wl,--defsym=RAM_LENGTH=${UNA_APP_GUI_RAM_LENGTH}
-        -Wl,-Map=${CMAKE_BINARY_DIR}/${TARGET_NAME}.elf.map
+        -Wl,-Map=${OUTPUT_PATH}/${TARGET_NAME}.elf.map
         ${UNA_APP_COMMON_LINK_OPTIONS}
-        -L${UNA_SDK}/libc++
+        -L$ENV{UNA_SDK}/libc++
     )
 
     add_custom_command(TARGET ${TARGET_NAME} POST_BUILD
-        COMMAND python3 ${UNA_SDK}/Utilities/Scripts/app_packer/app_packer.py -e $<TARGET_FILE:${TARGET_NAME}> -o ${CMAKE_CURRENT_BINARY_DIR}/Tmp -ext gui
+        COMMAND python3 $ENV{UNA_SDK}/Utilities/Scripts/app_packer/app_packer.py -e $<TARGET_FILE:${TARGET_NAME}> -o ${CMAKE_CURRENT_BINARY_DIR}/Tmp -ext gui
         COMMENT "Packing ${TARGET_NAME}"
     )
 endfunction()
 
 # Main function to build a complete watch app
 function(una_app_build_app)
-    if(DEFINED APP_PATH)
-        set(APP_ROOT "${APP_PATH}")
-    else()
-        get_filename_component(LEGACY_ROOT "${CMAKE_CURRENT_SOURCE_DIR}/../../.." ABSOLUTE)
-        if(EXISTS "${LEGACY_ROOT}/Software/Libs")
-            set(APP_ROOT "${LEGACY_ROOT}")
-        else()
-            set(APP_ROOT "${CMAKE_CURRENT_SOURCE_DIR}")
-        endif()
-    endif()
-
-    if(NOT IS_ABSOLUTE "${APP_ROOT}")
-        get_filename_component(APP_ROOT "${APP_ROOT}" ABSOLUTE BASE_DIR "${CMAKE_CURRENT_SOURCE_DIR}")
-    endif()
-
-    # Set up paths relative to app root unless overridden
-    if(NOT DEFINED LIBS_PATH)
-        if(EXISTS "${APP_ROOT}/Software/Libs")
-            set(LIBS_PATH "${APP_ROOT}/Software/Libs")
-        else()
-            set(LIBS_PATH "${APP_ROOT}/Libs")
-        endif()
-    endif()
-    if(NOT DEFINED TOUCHGFX_GUI_PATH)
-        if(EXISTS "${APP_ROOT}/Software/Apps/TouchGFX-GUI")
-            set(TOUCHGFX_GUI_PATH "${APP_ROOT}/Software/Apps/TouchGFX-GUI")
-        else()
-            set(TOUCHGFX_GUI_PATH "${APP_ROOT}/TouchGFX-GUI")
-        endif()
-    endif()
-    if(NOT DEFINED OUTPUT_PATH)
-        set(OUTPUT_PATH "${APP_ROOT}/Output")
-    endif()
-    if(NOT DEFINED RESOURCES_PATH)
-        set(RESOURCES_PATH "${APP_ROOT}/Resources")
-    endif()
-
-    set(SCRIPTS_PATH "${UNA_SDK}/Utilities/Scripts")
-
-    # Set up version
-    una_app_setup_version(BUILD_VERSION ${CMAKE_CURRENT_SOURCE_DIR})
-
-    # Make BUILD_VERSION available globally
-    set(BUILD_VERSION "${BUILD_VERSION}" PARENT_SCOPE)
-
-    # Build service and GUI
-    una_app_build_service(${APP_NAME}Service.elf ${LIBS_PATH} ${TOUCHGFX_GUI_PATH})
-
-    set(OUTPUT_PATHS "")
-    foreach(path IN LISTS OUTPUT_PATH)
-        if(IS_ABSOLUTE "${path}")
-            list(APPEND OUTPUT_PATHS "${path}")
-        else()
-            list(APPEND OUTPUT_PATHS "${CMAKE_CURRENT_SOURCE_DIR}/${path}")
-        endif()
-    endforeach()
-
-    if(DEFINED APP_TYPE AND APP_TYPE STREQUAL "service-only")
-        foreach(out_path IN LISTS OUTPUT_PATHS)
-            add_custom_command(TARGET ${APP_NAME}Service.elf POST_BUILD
-                COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_CURRENT_BINARY_DIR}/Tmp ${out_path}/Tmp
-                COMMENT "Copying service output"
-            )
-        endforeach()
-        return()
-    endif()
-
-    una_app_build_gui(${APP_NAME}GUI.elf ${LIBS_PATH} ${TOUCHGFX_GUI_PATH})
-
     set(OUTPUT_COPY_COMMANDS "")
-    foreach(out_path IN LISTS OUTPUT_PATHS)
+    foreach(files IN LISTS OUTPUT_DIR)
         list(APPEND OUTPUT_COPY_COMMANDS
-            COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_CURRENT_BINARY_DIR}/Tmp ${out_path}/Tmp
-            COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_BINARY_DIR}/${APP_NAME}_${BUILD_VERSION}.uapp ${out_path}/
+            COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_CURRENT_BINARY_DIR}/Tmp ${files}/Tmp
+            COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_BINARY_DIR}/${APP_NAME}_${BUILD_VERSION}.uapp ${files}/
         )
     endforeach()
 
--- cmake/una-sdk.cmake
+++ cmake/una-sdk.cmake
@@ -2,51 +2,51 @@
 # Extracted from una-app.cmake
 
 set(UNA_SDK_SOURCES_COMMON
-    "${UNA_SDK}/Libs/Source/AppSystem/AtExitImpl.cpp"
-    "${UNA_SDK}/Libs/Source/AppSystem/startup_user_app.s"
-    "${UNA_SDK}/Libs/Source/AppSystem/system.cpp"
-    "${UNA_SDK}/Libs/Source/Kernel/KernelBuilder.cpp"
-    "${UNA_SDK}/Libs/Source/UnaLogger/Logger.cpp"
+    "$ENV{UNA_SDK}/Libs/Source/AppSystem/AtExitImpl.cpp"
+    "$ENV{UNA_SDK}/Libs/Source/AppSystem/startup_user_app.s"
+    "$ENV{UNA_SDK}/Libs/Source/AppSystem/system.cpp"
+    "$ENV{UNA_SDK}/Libs/Source/Kernel/KernelBuilder.cpp"
+    "$ENV{UNA_SDK}/Libs/Source/UnaLogger/Logger.cpp"
 )
 
 set(UNA_SDK_SOURCES_SERVICE
-    "${UNA_SDK}/Libs/Source/AppSystem/EntryPoint/Service/main.cpp"
-    "${UNA_SDK}/Libs/Source/FitHelper/FitHelper.cpp"
-    "${UNA_SDK}/Libs/Source/JSON/JsonStreamReader.cpp"
-    "${UNA_SDK}/Libs/Source/JSON/JsonStreamWriter.cpp"
-    "${UNA_SDK}/Libs/Source/SensorLayer/SensorConnection.cpp"
-    "${UNA_SDK}/Libs/Source/TrackMap/TrackMapBuilder.cpp"
-    "${UNA_SDK}/ThirdParty/FitSDKRelease_21.171.00/c/fit.c"
-    "${UNA_SDK}/ThirdParty/FitSDKRelease_21.171.00/c/fit_convert.c"
-    "${UNA_SDK}/ThirdParty/FitSDKRelease_21.171.00/c/fit_crc.c"
-    "${UNA_SDK}/ThirdParty/FitSDKRelease_21.171.00/c/fit_product.c"
-    "${UNA_SDK}/ThirdParty/coreJSON/source/core_json.c"
+    "$ENV{UNA_SDK}/Libs/Source/AppSystem/EntryPoint/Service/main.cpp"
+    "$ENV{UNA_SDK}/Libs/Source/FitHelper/FitHelper.cpp"
+    "$ENV{UNA_SDK}/Libs/Source/JSON/JsonStreamReader.cpp"
+    "$ENV{UNA_SDK}/Libs/Source/JSON/JsonStreamWriter.cpp"
+    "$ENV{UNA_SDK}/Libs/Source/SensorLayer/SensorConnection.cpp"
+    "$ENV{UNA_SDK}/Libs/Source/TrackMap/TrackMapBuilder.cpp"
+    "$ENV{UNA_SDK}/ThirdParty/FitSDKRelease_21.171.00/c/fit.c"
+    "$ENV{UNA_SDK}/ThirdParty/FitSDKRelease_21.171.00/c/fit_convert.c"
+    "$ENV{UNA_SDK}/ThirdParty/FitSDKRelease_21.171.00/c/fit_crc.c"
+    "$ENV{UNA_SDK}/ThirdParty/FitSDKRelease_21.171.00/c/fit_product.c"
+    "$ENV{UNA_SDK}/ThirdParty/coreJSON/source/core_json.c"
 )
 
 set(UNA_SDK_SOURCES_GUI
-    "${UNA_SDK}/Libs/Source/AppSystem/EntryPoint/TouchGFX/main.cpp"
-    "${UNA_SDK}/Port/TouchGFX/STM32TouchController.cpp"
-    "${UNA_SDK}/Port/TouchGFX/TouchGFXCommandProcessor.cpp"
-    "${UNA_SDK}/Port/TouchGFX/TouchGFXGPIO.cpp"
-    "${UNA_SDK}/Port/TouchGFX/TouchGFXHAL.cpp"
-    "${UNA_SDK}/Port/TouchGFX/generated/OSWrappers.cpp"
-    "${UNA_SDK}/Port/TouchGFX/generated/STM32DMA.cpp"
-    "${UNA_SDK}/Port/TouchGFX/generated/TouchGFXConfiguration.cpp"
-    "${UNA_SDK}/Port/TouchGFX/generated/TouchGFXGeneratedHAL.cpp"
+    "$ENV{UNA_SDK}/Libs/Source/AppSystem/EntryPoint/TouchGFX/main.cpp"
+    "$ENV{UNA_SDK}/Port/TouchGFX/STM32TouchController.cpp"
+    "$ENV{UNA_SDK}/Port/TouchGFX/TouchGFXCommandProcessor.cpp"
+    "$ENV{UNA_SDK}/Port/TouchGFX/TouchGFXGPIO.cpp"
+    "$ENV{UNA_SDK}/Port/TouchGFX/TouchGFXHAL.cpp"
+    "$ENV{UNA_SDK}/Port/TouchGFX/generated/OSWrappers.cpp"
+    "$ENV{UNA_SDK}/Port/TouchGFX/generated/STM32DMA.cpp"
+    "$ENV{UNA_SDK}/Port/TouchGFX/generated/TouchGFXConfiguration.cpp"
+    "$ENV{UNA_SDK}/Port/TouchGFX/generated/TouchGFXGeneratedHAL.cpp"
 )
 
 set(UNA_SDK_INCLUDE_DIRS_COMMON
-    "${UNA_SDK}/Libs/Header"
+    "$ENV{UNA_SDK}/Libs/Header"
 )
 
 set(UNA_SDK_INCLUDE_DIRS_SERVICE
-    "${UNA_SDK}/ThirdParty/FitSDKRelease_21.171.00/c"
-    "${UNA_SDK}/ThirdParty/coreJSON/source/include"
+    "$ENV{UNA_SDK}/ThirdParty/FitSDKRelease_21.171.00/c"
+    "$ENV{UNA_SDK}/ThirdParty/coreJSON/source/include"
 )
 
 set(UNA_SDK_INCLUDE_DIRS_GUI
-    "${UNA_SDK}/Port/TouchGFX"
-    "${UNA_SDK}/Port/TouchGFX/generated"
+    "$ENV{UNA_SDK}/Port/TouchGFX"
+    "$ENV{UNA_SDK}/Port/TouchGFX/generated"
 )
 
-set(SCRIPTS_PATH "${UNA_SDK}/Utilities/Scripts")
+set(SCRIPTS_PATH "$ENV{UNA_SDK}/Utilities/Scripts")
