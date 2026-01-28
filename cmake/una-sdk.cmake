38182fb601f4d6ed16224d18ada477b3433cb546
avatarsd <avatarsd2@gmail.com>
2026-01-28 21:39:31 +0200

    build(cmake): refine configuration to match CubeIDE exactly
    - Remove newlib-nano specs from toolchain init flags
    - Enable ASM language with appropriate flags
    - Scope compile options to C/CXX languages only
    - Enable verbose makefiles
    - Modularize UNA SDK sources/includes with backward compatibility
    - Fix linker path quoting and library references

--- cmake/una-sdk.cmake
+++ cmake/una-sdk.cmake
@@ -9,18 +9,39 @@ set(UNA_SDK_SOURCES_COMMON
     "$ENV{UNA_SDK}/Libs/Source/UnaLogger/Logger.cpp"
 )
 
-set(UNA_SDK_SOURCES_SERVICE
+set(UNA_SDK_SOURCES_APPSYSTEM
     "$ENV{UNA_SDK}/Libs/Source/AppSystem/EntryPoint/Service/main.cpp"
+)
+
+set(UNA_SDK_SOURCES_FIT
     "$ENV{UNA_SDK}/Libs/Source/FitHelper/FitHelper.cpp"
-    "$ENV{UNA_SDK}/Libs/Source/JSON/JsonStreamReader.cpp"
-    "$ENV{UNA_SDK}/Libs/Source/JSON/JsonStreamWriter.cpp"
-    "$ENV{UNA_SDK}/Libs/Source/SensorLayer/SensorConnection.cpp"
-    "$ENV{UNA_SDK}/Libs/Source/TrackMap/TrackMapBuilder.cpp"
     "$ENV{UNA_SDK}/ThirdParty/FitSDKRelease_21.171.00/c/fit.c"
     "$ENV{UNA_SDK}/ThirdParty/FitSDKRelease_21.171.00/c/fit_convert.c"
     "$ENV{UNA_SDK}/ThirdParty/FitSDKRelease_21.171.00/c/fit_crc.c"
     "$ENV{UNA_SDK}/ThirdParty/FitSDKRelease_21.171.00/c/fit_product.c"
+)
+
+set(UNA_SDK_SOURCES_JSON
+    "$ENV{UNA_SDK}/Libs/Source/JSON/JsonStreamReader.cpp"
+    "$ENV{UNA_SDK}/Libs/Source/JSON/JsonStreamWriter.cpp"
     "$ENV{UNA_SDK}/ThirdParty/coreJSON/source/core_json.c"
+)
+
+set(UNA_SDK_SOURCES_SENSOR
+    "$ENV{UNA_SDK}/Libs/Source/SensorLayer/SensorConnection.cpp"
+)
+
+set(UNA_SDK_SOURCES_TRACKMAP
+    "$ENV{UNA_SDK}/Libs/Source/TrackMap/TrackMapBuilder.cpp"
+)
+
+# Combined service sources for backward compatibility
+set(UNA_SDK_SOURCES_SERVICE
+    "${UNA_SDK_SOURCES_APPSYSTEM}"
+    "${UNA_SDK_SOURCES_FIT}"
+    "${UNA_SDK_SOURCES_JSON}"
+    "${UNA_SDK_SOURCES_SENSOR}"
+    "${UNA_SDK_SOURCES_TRACKMAP}"
 )
 
 set(UNA_SDK_SOURCES_GUI
@@ -39,14 +60,23 @@ set(UNA_SDK_INCLUDE_DIRS_COMMON
     "$ENV{UNA_SDK}/Libs/Header"
 )
 
-set(UNA_SDK_INCLUDE_DIRS_SERVICE
+set(UNA_SDK_INCLUDE_DIRS_FIT
     "$ENV{UNA_SDK}/ThirdParty/FitSDKRelease_21.171.00/c"
+)
+
+set(UNA_SDK_INCLUDE_DIRS_JSON
     "$ENV{UNA_SDK}/ThirdParty/coreJSON/source/include"
 )
 
 set(UNA_SDK_INCLUDE_DIRS_GUI
     "$ENV{UNA_SDK}/Libs/include/Port/TouchGFX"
     "$ENV{UNA_SDK}/Libs/include/Port/TouchGFX/generated"
+)
+
+# Combined service includes for backward compatibility
+set(UNA_SDK_INCLUDE_DIRS_SERVICE
+    "${UNA_SDK_INCLUDE_DIRS_FIT}"
+    "${UNA_SDK_INCLUDE_DIRS_JSON}"
 )
 
 set(SCRIPTS_PATH "$ENV{UNA_SDK}/Utilities/Scripts")
