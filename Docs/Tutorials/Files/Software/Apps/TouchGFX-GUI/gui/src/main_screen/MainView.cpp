4fff3f6261db6470ec02ec707eadbce0226e720d
avatarsd <avatarsd2@gmail.com>
2026-03-16 21:59:52 +0200

    chore(tutorials): add Files tutorial scaffolding

---
+++ Docs/Tutorials/Files/Software/Apps/TouchGFX-GUI/gui/src/main_screen/MainView.cpp
@@ -0,0 +1,40 @@
+#include <gui/main_screen/MainView.hpp>
+
+MainView::MainView()
+{
+
+}
+
+void MainView::setupScreen()
+{
+    MainViewBase::setupScreen();
+
+    buttons.setL1(ButtonsSet::NONE);
+    buttons.setL2(ButtonsSet::NONE);
+    buttons.setR1(ButtonsSet::NONE);
+    buttons.setR2(ButtonsSet::AMBER);
+}
+
+void MainView::tearDownScreen()
+{
+    MainViewBase::tearDownScreen();
+}
+
+void MainView::handleKeyEvent(uint8_t key)
+{
+    if (key == Gui::Config::Button::L1) {
+
+    }
+
+    if (key == Gui::Config::Button::L2) {
+
+    }
+
+    if (key == Gui::Config::Button::R1) {
+   
+    }
+
+    if (key == Gui::Config::Button::R2) {
+        presenter->exit();
+    }
+}
